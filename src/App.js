20d1545f67e109a200f88b0abc2c734849181854
firefrost <f20170622@pilani.bits-pilani.ac.in>
2020-05-17 20:02:39 +0530

    Changed API to https

--- src/App.js
+++ src/App.js
@@ -2,53 +2,53 @@ import React, { Component } from 'react';
 import Titles from "./components/titles"
 import Form from "./components/form"
 import Weather from "./components/weather"
-import "./App.css" ; 
+import "./App.css" ;
 
-	
+
 const API_KEY = "fb53cadbf27b64914085122652d9ab07"
 
 class App extends Component {
 
   state={
- temperature : undefined , 
- city: undefined , 
- country : undefined , 
- humidity : undefined , 
- description : undefined , 
- error : undefined , 
+ temperature : undefined ,
+ city: undefined ,
+ country : undefined ,
+ humidity : undefined ,
+ description : undefined ,
+ error : undefined ,
 
   }
-  
+
 getWeather= async(e) => {
-  e.preventDefault() ; 
+  e.preventDefault() ;
   const city = e.target.elements.city.value ;
   const country = e.target.elements.country.value ;
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`) ; 
-const data = await api_call.json() ; 
+  const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`) ; 
+const data = await api_call.json() ;
 
 if(city && country) {
-  console.log(data) ;  
+  console.log(data) ;
 this.setState ({
-  temperature : data.main.temp , 
-  city : data.name , 
-  country : data.sys.country , 
-  humidity : data.main.humidity , 
-  description : data.weather[0].description , 
+  temperature : data.main.temp ,
+  city : data.name ,
+  country : data.sys.country ,
+  humidity : data.main.humidity ,
+  description : data.weather[0].description ,
   error : " "
 })
 }
 else {this.setState ({
-  temperature : undefined, 
-  city : undefined , 
-  country : undefined , 
-  humidity : undefined , 
-  description : undefined , 
-  error : " Please Enter Values " , 
+  temperature : undefined,
+  city : undefined ,
+  country : undefined ,
+  humidity : undefined ,
+  description : undefined ,
+  error : " Please Enter Values " ,
 })
 }
 }
-  render() { 
-    return (  
+  render() {
+    return (
 
 <div>
   <div className="wrapper">
@@ -59,7 +59,7 @@ else {this.setState ({
           < Titles />  </div>
           <div className="col -xs-7 form-container">
           <Form  getWeather={this.getWeather}/>
-<Weather 
+<Weather
 temperature = {this.state.temperature}
 city = {this.state.city}
 country={this.state.country}
@@ -67,7 +67,7 @@ humidity={this.state.humidity}
 description={this.state.description}
 error={this.state.error} />
 
-            
+
             </div>
         </div>
       </div>
@@ -83,5 +83,5 @@ error={this.state.error} />
     );
   }
 }
- 
+
 export default App;
