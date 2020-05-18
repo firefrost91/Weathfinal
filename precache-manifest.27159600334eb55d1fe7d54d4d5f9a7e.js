0dbc6a2a719de3b6e8542fa46386bf514bc3803e
firefrost <f20170622@pilani.bits-pilani.ac.in>
2020-05-18 10:23:09 +0530

    Updates

---
+++ precache-manifest.27159600334eb55d1fe7d54d4d5f9a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc5af740683d86ccc8961db56545bcd",
+    "url": "/Weathfinal/index.html"
+  },
+  {
+    "revision": "0fd3c26cd8ba1bc48102",
+    "url": "/Weathfinal/static/css/2.b0443ea5.chunk.css"
+  },
+  {
+    "revision": "7be632dee0400522de89",
+    "url": "/Weathfinal/static/css/main.98e20755.chunk.css"
+  },
+  {
+    "revision": "0fd3c26cd8ba1bc48102",
+    "url": "/Weathfinal/static/js/2.34decb7f.chunk.js"
+  },
+  {
+    "revision": "7be632dee0400522de89",
+    "url": "/Weathfinal/static/js/main.e7150076.chunk.js"
+  },
+  {
+    "revision": "02c711fe22000becd4a1",
+    "url": "/Weathfinal/static/js/runtime~main.258c3178.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Weathfinal/static/media/bg.b6796c73.jpg"
+  }
+]);
